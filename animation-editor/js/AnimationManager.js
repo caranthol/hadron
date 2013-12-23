d14623cc9b142de5177f7f322d745207a4a2a25e
caranthol <caranthol@gmail.com>
2013-12-23 19:53:46 +0100

    Modificamos el método de pintado para adaptarlo a los loop que hemos incorporado.

--- animation-editor/js/AnimationManager.js
+++ animation-editor/js/AnimationManager.js
@@ -1,193 +1,283 @@
 'use strict';
-var canvas = document.getElementById('myCanvas');
-var context = canvas.getContext('2d');
-
 
 function Animation()
 {   
-	this.frameW;
-	this.frameH;
+    this.fps = 30;
 
-	this.cols = 1;
-	this.rows = 1;
+    this.startCol = 0;
+    this.startRow = 0;
+    this.endCol = 0;
+    this.endRow = 0;
+    this.currentCol = 0;
+    this.currentRow = 0;
 
-	this.fps = 30;
-
-	this.startX = 1;
-	this.startY = 1;
-	this.endX = 1;
-	this.endY = 1;
-	this.currentX = 1;
-	this.currentY = 1;
-
-	this.xDef = 1;
-	this.yDef = 1;
-
-	this.loop = 'default';
-	this.dir = 1;
+    this.xDef = 1;
+    this.yDef = 1;
+    
+    this.loop = 'default';
+    this.dir = 1;
 }
 
 Animation.prototype.initAnimationFromJSON = function(json)
 {
-	var jsonObj = JSON.parse(json);
-	this.frameW = jsonObj.frameW;
-	this.frameH = jsonObj.frameH;
-	this.currentX = jsonObj.currentX;
-	this.currentY = jsonObj.currentY;
-	this.cols = jsonObj.cols;
-	this.rows =jsonObj. rows;
-	this.fps = jsonObj.fps;
-	this.xDef = jsonObj.xDef;
-	this.yDef = jsonObj.yDef;
-	this.startX = jsonObj.startX;
-	this.startY = jsonObj.startY;
-	this.endX = jsonObj.endX;
-	this.endY = jsonObj.endY;
-	this.loop = jsonObj.loop;
-	this.dir = jsonObj.dir;
+    var jsonObj = JSON.parse(json);
+    this.currentCol = jsonObj.currentCol;
+    this.currentRow = jsonObj.currentRow;
+    this.fps = jsonObj.fps;
+    this.xDef = jsonObj.xDef;
+    this.yDef = jsonObj.yDef;
+    this.startCol = jsonObj.startCol;
+    this.startRow = jsonObj.startRow;
+    this.endCol = jsonObj.endCol;
+    this.endRow = jsonObj.endRow;
+    this.loop = jsonObj.loop;
+    this.dir = jsonObj.dir;
 };
 
 function AnimationManager()
 {   
-	this.image;
-	this.imageName;
-	this.currentframe = 0;
-	this.timeBetweenframes;
-	this.timeSinceLastframe;
-	this.animations = [];
-	this.currentAnimation;
+    this.image;
+    this.imageName;
+    this.frameW;
+    this.frameH;
+    this.cols = 1;
+    this.rows = 1;
+    this.timeBetweenframes;
+    this.timeSinceLastframe;
+    this.animations = [];
+    this.currentAnimation;
 }
 
 AnimationManager.prototype.initAnimationManagerFromJSON = function(json)
 {
-	var jsonObj = JSON.parse(json);
-	this.image = jsonObj.image;
-	this.imageName = jsonObj.imageName;
-	this.currentframe = jsonObj.currentframe;
-	this.timeBetweenframes = jsonObj.timeBetweenframes;
-	this.timeSinceLastframe = jsonObj.timeSinceLastframe;
-	this.currentAnimation = jsonObj.currentAnimation;
+    var jsonObj = JSON.parse(json);
+    this.image = jsonObj.image;
+    this.imageName = jsonObj.imageName;
+    this.frameW = jsonObj.frameW;
+    this.frameH = jsonObj.frameH;
+    this.cols = jsonObj.cols;
+    this.rows = jsonObj.rows;
+    this.timeBetweenframes = jsonObj.timeBetweenframes;
+    this.timeSinceLastframe = jsonObj.timeSinceLastframe;
+    this.currentAnimation = jsonObj.currentAnimation;
 };
 
 AnimationManager.prototype.addAnimation = function(name, animation)
 {
-	this.animations[name] = animation;
+    this.animations[name] = animation;
 };
 
 AnimationManager.prototype.setAnimation = function(name)
 {
-	this.currentframe = 0;
-	this.timeBetweenframes = 1/this.animations[name].fps;
-	this.timeSinceLastframe = this.timeBetweenframes;
-	this.currentAnimation = name;
+    this.timeBetweenframes = 1/this.animations[name].fps;
+    this.timeSinceLastframe = this.timeBetweenframes;
+    this.currentAnimation = name;
+    this.animations[name].currentCol = this.animations[name].startCol;
+    this.animations[name].currentRow = this.animations[name].startRow;
 };
 
 AnimationManager.prototype.draw = function(dt, context, x, y)
 {
-	var sourceX = this.frameW * this.currentframe;
-	var animation = this.animations[currentAnimation];
-	var destW = animation.xDef*animation.frameW;
-	var destH = animation.yDef*animation.frameH;
-	context.drawImage(this.image, sourceX, animation.frameY, animation.frameW, animation.frameH, x, y, destW, destH);
+    var animation = this.animations[currentAnimation];
+    var destW = animation.xDef*this.frameW;
+    var destH = animation.yDef*this.frameH;
 
-	this.timeSinceLastframe -= dt;
+    var sourceX = this.frameW * animation.currentCol;
+    var sourceY = this.frameH * animation.currentRow;
 
-	if (this.timeSinceLastframe <= 0)
-	{
-		this.timeSinceLastframe = this.timeBetweenframes;
-		++this.currentframe;
-		this.currentframe %= animation.cols;
-	}
+    context.drawImage(this.image, sourceX, sourceY, this.frameW,
+                    this.frameH, x, y, destW, destH);
+
+    this.timeSinceLastframe -= dt;
+
+    if (this.timeSinceLastframe <= 0)
+    {
+        this.timeSinceLastframe = this.timeBetweenframes;
+        if(animation.loop == 'default'){
+            if (animation.currentRow < animation.endRow){
+                if (animation.currentCol < this.cols)
+                	animation.currentCol += 1;
+                else{
+                	animation.currentCol = 0;
+                	if(animation.currentRow < this.rows)
+                		animation.currentRow +=1;
+                	else
+                		animation.currentRow = 0;
+                }
+
+            }
+            else if (animation.currentRow == animation.endRow){
+                if(animation.currentCol < animation.endCol)
+                	animation.currentCol += 1;
+                else{
+                	animation.currentCol = animation.startCol;
+                	animation.currentRow = animation.startRow;
+                }
+            }
+        }
+        else if (animation.loop == 'reverse'){
+            if (animation.dir == 1){
+                if (animation.currentRow < animation.endRow){
+                    if (animation.currentCol < this.cols)
+                    	animation.currentCol += 1;
+                    else{
+                    	animation.currentCol = 0;
+                    	if(animation.currentRow < this.rows)
+                    		animation.currentRow +=1;
+                    	else
+                    		animation.currentRow = 0;
+                    }
+                }
+                else if (animation.currentRow == animation.endRow){
+                    if(animation.currentCol < animation.endCol)
+                    	animation.currentCol += 1;
+                    else{
+                        if(animation.currentCol > 0){
+                        	animation.currentCol -= 1;
+                        	animation.dir = -1;
+                        }
+                        else{
+                        	animation.currentCol = this.cols;
+                        	if(animation.currentRow > 0)
+                        		animation.currentRow -=1;
+                        	else
+                        		animation.currentRow = this.rows;
+                        	animation.dir = -1;
+                        }
+                    }
+                }
+            }
+            else if (animation.dir == -1){
+                if (animation.currentRow > animation.startRow){
+                    if (animation.currentCol > 0)
+                    	animation.currentCol -= 1;
+                    else{
+                    	animation.currentCol = this.cols;
+                    	if(animation.currentRow > 0)
+                    		animation.currentRow -=1;
+                    	else
+                    		animation.currentRow = this.rows;
+                    }
+                }
+                else if (animation.currentRow == animation.startRow){
+                    if(animation.currentCol > animation.startCol)
+                    	animation.currentCol -= 1;
+                    else{
+                        if(animation.currentCol < this.cols){
+                        	animation.currentCol += 1;
+                        	animation.dir = 1;
+                        }
+                        else{
+                        	animation.currentCol = 0;
+                        	if(animation.currentRow < this.rows)
+                        		animation.currentRow +=1;
+                        	else
+                        		animation.currentRow = 0;
+                        	animation.dir = 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
 };
-
-var imageObj = new Image();
 
 AnimationManager.prototype.drawImmediate = function(context, x, y)
 {
-	imageObj.onload = function() {
-		var animation = this.animations[currentAnimation];
-		var destW = animation.xDef*animation.frameW;
-		var destH = animation.yDef*animation.frameH;
+    var animation = this.animations[this.currentAnimation];
+    var destW = animation.xDef*this.frameW;
+    var destH = animation.yDef*this.frameH;
 
-		var sourceX = this.frameW * this.currentX;
-		var sourceY = this.frameH * this.currentY;
+    var sourceX = this.frameW * animation.currentCol;
+    var sourceY = this.frameH * animation.currentRow;
 
-		context.drawImage(this.image, sourceX, sourceY, animation.frameW,
-				animation.frameH, x, y, destW, destH);
+    context.drawImage(this.image, sourceX, sourceY, this.frameW,
+                    this.frameH, x, y, destW, destH);
+    
+    if(animation.loop == 'default'){
+        if (animation.currentRow != animation.endRow){
+            if (animation.currentCol < this.cols)
+            	animation.currentCol += 1;
+            else{
+            	animation.currentCol = 0;
+            	if(animation.currentRow < this.rows)
+            		animation.currentRow +=1;
+            	else
+            		animation.currentRow = 0;
+            }
 
-		if(loop == 'default'){
-			if (currentY < destY){
-				if (currentX <= cols)
-					currentX += 1;
-				else{
-					currentX = 1;
-					currentY +=1;
-				}
-
-			}
-			else if (currentY == destY){
-				if(currentX < destX)
-					currentX += 1;
-				else{
-					currentX = startX;
-					currentY = startY;
-				}
-			}
-		}
-		else if (loop == 'reverse'){
-			if (dir == 1){
-				if (currentY < destY){
-					if (currentX <= cols)
-						currentX += 1;
-					else{
-						currentX = 1;
-						currentY +=1;
-					}
-
-				}
-				else if (currentY == destY){
-					if(currentX < destX)
-						currentX += 1;
-					else{
-						if(currentX > 1){
-							currentX -= 1;
-							dir == -1;
-						}
-						else{
-							currentX = cols;
-							currentY -= 1;
-							dir == -1;
-						}
-					}
-				}
-			}
-			else{
-				if (currentY > startY){
-					if (currentX > 1)
-						currentX -= 1;
-					else{
-						currentX = cols;
-						currentY -=1;
-					}
-
-				}
-				else if (currentY == startY){
-					if(currentX > startX)
-						currentX -= 1;
-					else{
-						if(currentX < cols){
-							currentX += 1;
-							dir = 1;
-						}
-						else{
-							currentX = 1;
-							currentY += 1;
-							dir == 1;
-						}
-					}
-				}
-			}
-		}
-	};
-	imageObj.src = AnimationManager.image;
+        }
+        else if (animation.currentRow == animation.endRow){
+            if(animation.currentCol < animation.endCol)
+            	animation.currentCol += 1;
+            else{
+            	animation.currentCol = animation.startCol;
+            	animation.currentRow = animation.startRow;
+            }
+        }
+    }
+    else if (animation.loop == 'reverse'){
+        if (animation.dir == 1){
+            if (animation.currentRow != animation.endRow){
+                if (animation.currentCol < this.cols)
+                	animation.currentCol += 1;
+                else{
+                	animation.currentCol = 0;
+                	if(animation.currentRow < this.rows)
+                		animation.currentRow +=1;
+                	else
+                		animation.currentRow = 0;
+                }
+            }
+            else if (animation.currentRow == animation.endRow){
+                if(animation.currentCol < animation.endCol)
+                	animation.currentCol += 1;
+                else{
+                    if(animation.currentCol > 0){
+                    	animation.currentCol -= 1;
+                    	animation.dir = -1;
+                    }
+                    else{
+                    	animation.currentCol = this.cols;
+                    	if(animation.currentRow > 0)
+                    		animation.currentRow -=1;
+                    	else
+                    		animation.currentRow = this.rows;
+                    	animation.dir = -1;
+                    }
+                }
+            }
+        }
+        else if (animation.dir == -1){
+            if (animation.currentRow != animation.startRow){
+                if (animation.currentCol > 0)
+                	animation.currentCol -= 1;
+                else{
+                	animation.currentCol = this.cols;
+                	if(animation.currentRow > 0)
+                		animation.currentRow -=1;
+                	else
+                		animation.currentRow = this.rows;
+                }
+            }
+            else if (animation.currentRow == animation.startRow){
+                if(animation.currentCol > animation.startCol)
+                	animation.currentCol -= 1;
+                else{
+                    if(animation.currentCol < this.cols){
+                    	animation.currentCol += 1;
+                    	animation.dir = 1;
+                    }
+                    else{
+                    	animation.currentCol = 0;
+                    	if(animation.currentRow < this.rows)
+                    		animation.currentRow +=1;
+                    	else
+                    		animation.currentRow = 0;
+                    	animation.dir = 1;
+                    }
+                }
+            }
+        }
+    }
 };
