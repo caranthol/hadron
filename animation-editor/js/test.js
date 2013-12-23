c4b91539cc7d0c835cd80c60f0ff3e50c7f79941
caranthol <caranthol@gmail.com>
2013-12-23 19:54:13 +0100

    Se elimina el texto comentado en el html. Se adapta a las llamadas con loop.

--- animation-editor/js/test.js
+++ animation-editor/js/test.js
@@ -4,7 +4,7 @@ var image = new Image();
 var ani1 = new Animation();
 var ani2 = new Animation();
 var ani3 = new Animation();
-var ms = 100;
+var ms = 500;
 var xSprites = 31;
 var ySprites = 21;
 var imgLoaded = false;
@@ -12,50 +12,51 @@ var imgLoaded = false;
 image.src = 'imgs/anim2.png';
 
 image.onload = function(){
-	var w, h;
-	w = this.width/xSprites;
-	h = this.height/ySprites;
+        var w, h;
+        w = this.width/xSprites;
+        h = this.height/ySprites;
 
-	ani1.startCol = 0;
-	ani1.startRow = 0;
-	ani1.endCol = 4;
-	ani1.endRow = 0;
-	ani1.fps = ms;
+        ani1.startCol = 0;
+        ani1.startRow = 0;
+        ani1.endCol = 4;
+        ani1.endRow = 0;
+        ani1.fps = ms;
 
-	ani2.startCol = 7;
-	ani2.startRow = 4;
-	ani2.endCol = 15;
-	ani2.endRow = 5;
-	ani2.fps = ms;
+        ani2.startCol = 0;
+        ani2.startRow = 0;
+        ani2.endCol = 15;
+        ani2.endRow = 5;
+        ani2.fps = ms;
 
-	ani3.startCol = 27;
-	ani3.startRow = 19;
-	ani3.endCol = 30;
-	ani3.endRow = 20;
-	ani3.fps = ms;
+        ani3.startCol = 28;
+        ani3.startRow = 20;
+        ani3.endCol = 1;
+        ani3.endRow = 0;
+        ani3.fps = ms;
+        ani3.loop='reverse';
 
-	animationManager.cols = xSprites;
-	animationManager.rows = ySprites;
-	animationManager.frameH = h;
-	animationManager.frameW = w;
-	animationManager.image = this;
-	animationManager.imageName = this.name;
-	animationManager.addAnimation("1", ani1);
-	animationManager.addAnimation("2", ani2);
-	animationManager.addAnimation("3", ani3);
-	animationManager.setAnimation("3");
-	
-	imgLoaded = true;
+        animationManager.cols = xSprites - 1;
+        animationManager.rows = ySprites - 1;
+        animationManager.frameH = h;
+        animationManager.frameW = w;
+        animationManager.image = this;
+        animationManager.imageName = this.name;
+        animationManager.addAnimation("1", ani1);
+        animationManager.addAnimation("2", ani2);
+        animationManager.addAnimation("3", ani3);
+        animationManager.setAnimation("3");
+        
+        imgLoaded = true;
 };
 
 /*while(imgLoaded == false){
-	
+        
 }*/
 draw = function(){
-	if(imgLoaded == true){
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		animationManager.drawImmediate(context,0,0);
-	}
+        if(imgLoaded == true){
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                animationManager.drawImmediate(context,0,0);
+        }
 };
-var simulation = setInterval(draw, 100);
+var simulation = setInterval(draw, ms);
 //animationManager.drawImmediate(context,0,0);
