25f0efee0808360c19f66eb2172758f760247b0d
caranthol <caranthol@gmail.com>
2013-12-23 11:42:42 +0100

    Se añaden los loop y las coordenadas de inicio y fin de la animación

--- animation-editor/js/AnimationManager.js
+++ animation-editor/js/AnimationManager.js
@@ -1,120 +1,193 @@
+'use strict';
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+
+
 function Animation()
 {   
-    this.frameW = 0;
-    this.frameH = 0;
-    this.frameY = 0;
-    this.cols = 0;
-    this.rows = 0;
-    this.fps = 0;
-    this.startX = 0;
-    this.startY = 0;
-    this.endX = 0;
-    this.endY = 0;
-    this.xDef = 1;
-    this.yDef = 1;
-}
+	this.frameW;
+	this.frameH;
 
-Animation.prototype.initAnimation = function(frameW, frameH, frameY, cols, rows, fps, startX, startY, endX, endY)
-{
-    this.frameW = frameW;
-    this.frameH = frameH;
-    this.frameY = frameY;
-    this.cols = cols;
-    this.rows = rows;
-    this.fps = fps;
-    this.startX = startX;
-    this.startY = startY;
-    this.endX = endX;
-    this.endY = endY;
-};
+	this.cols = 1;
+	this.rows = 1;
+
+	this.fps = 30;
+
+	this.startX = 1;
+	this.startY = 1;
+	this.endX = 1;
+	this.endY = 1;
+	this.currentX = 1;
+	this.currentY = 1;
+
+	this.xDef = 1;
+	this.yDef = 1;
+
+	this.loop = 'default';
+	this.dir = 1;
+}
 
 Animation.prototype.initAnimationFromJSON = function(json)
 {
 	var jsonObj = JSON.parse(json);
-    this.frameW = jsonObj.frameW;
-    this.frameH = jsonObj.frameH;
-    this.frameY = jsonObj.frameY;
-    this.cols = jsonObj.cols;
-    this.rows =jsonObj. rows;
-    this.fps = jsonObj.fps;
-    this.destX = jsonObj.destX;
-    this.destY = jsonObj.destY;
-    this.xDef = jsonObj.xDef;
-    this.yDef = jsonObj.yDef;
-    this.startX = jsonObj.startX;
-    this.startY = jsonObj.startY;
-    this.endX = jsonObj.endX;
-    this.endY = jsonObj.endY;
+	this.frameW = jsonObj.frameW;
+	this.frameH = jsonObj.frameH;
+	this.currentX = jsonObj.currentX;
+	this.currentY = jsonObj.currentY;
+	this.cols = jsonObj.cols;
+	this.rows =jsonObj. rows;
+	this.fps = jsonObj.fps;
+	this.xDef = jsonObj.xDef;
+	this.yDef = jsonObj.yDef;
+	this.startX = jsonObj.startX;
+	this.startY = jsonObj.startY;
+	this.endX = jsonObj.endX;
+	this.endY = jsonObj.endY;
+	this.loop = jsonObj.loop;
+	this.dir = jsonObj.dir;
 };
 
 function AnimationManager()
 {   
-    this.image = null;
-    this.imageName = null;
-    this.currentframe = 0;
-    this.timeBetweenframes = 0;
-    this.timeSinceLastframe = 0;
-    this.animations = [];
-    this.currentAnimation = null;
+	this.image;
+	this.imageName;
+	this.currentframe = 0;
+	this.timeBetweenframes;
+	this.timeSinceLastframe;
+	this.animations = [];
+	this.currentAnimation;
 }
-
-AnimationManager.prototype.initAnimationManager = function(image)
-{
-    this.image = image;
-    this.imageName = image.name;
-};
 
 AnimationManager.prototype.initAnimationManagerFromJSON = function(json)
 {
 	var jsonObj = JSON.parse(json);
-    this.image = jsonObj.image;
-    this.imageName = jsonObj.imageName;
-    this.currentframe = jsonObj.currentframe;
-    this.timeBetweenframes = jsonObj.timeBetweenframes;
-    this.timeSinceLastframe = jsonObj.timeSinceLastframe;
-    this.currentAnimation = jsonObj.currentAnimation;
+	this.image = jsonObj.image;
+	this.imageName = jsonObj.imageName;
+	this.currentframe = jsonObj.currentframe;
+	this.timeBetweenframes = jsonObj.timeBetweenframes;
+	this.timeSinceLastframe = jsonObj.timeSinceLastframe;
+	this.currentAnimation = jsonObj.currentAnimation;
 };
 
 AnimationManager.prototype.addAnimation = function(name, animation)
 {
-    this.animations[name] = animation;
+	this.animations[name] = animation;
 };
 
 AnimationManager.prototype.setAnimation = function(name)
 {
-    this.currentframe = 0;
-    this.timeBetweenframes = 1/this.animations[name].fps;
-    this.timeSinceLastframe = this.timeBetweenframes;
-    this.currentAnimation = name;
+	this.currentframe = 0;
+	this.timeBetweenframes = 1/this.animations[name].fps;
+	this.timeSinceLastframe = this.timeBetweenframes;
+	this.currentAnimation = name;
 };
 
 AnimationManager.prototype.draw = function(dt, context, x, y)
 {
-    var sourceX = this.frameW * this.currentframe;
-    var animation = this.animations[currentAnimation];
+	var sourceX = this.frameW * this.currentframe;
+	var animation = this.animations[currentAnimation];
 	var destW = animation.xDef*animation.frameW;
 	var destH = animation.yDef*animation.frameH;
-    context.drawImage(this.image, sourceX, animation.frameY, animation.frameW, animation.frameH, x, y, destW, destH);
+	context.drawImage(this.image, sourceX, animation.frameY, animation.frameW, animation.frameH, x, y, destW, destH);
 
-    this.timeSinceLastframe -= dt;
+	this.timeSinceLastframe -= dt;
 
-    if (this.timeSinceLastframe <= 0)
-    {
-       this.timeSinceLastframe = this.timeBetweenframes;
-       ++this.currentframe;
-       this.currentframe %= animation.cols;
-    }
+	if (this.timeSinceLastframe <= 0)
+	{
+		this.timeSinceLastframe = this.timeBetweenframes;
+		++this.currentframe;
+		this.currentframe %= animation.cols;
+	}
 };
+
+var imageObj = new Image();
 
 AnimationManager.prototype.drawImmediate = function(context, x, y)
 {
-    var sourceX = this.frameW * this.currentframe;
-    var animation = this.animations[currentAnimation];
-	var destW = animation.xDef*animation.frameW;
-	var destH = animation.yDef*animation.frameH;
-    context.drawImage(this.image, sourceX, animation.frameY, animation.frameW, animation.frameH, x, y, destW, destH);
-    ++this.currentframe;
-    this.currentframe %= animation.cols;
+	imageObj.onload = function() {
+		var animation = this.animations[currentAnimation];
+		var destW = animation.xDef*animation.frameW;
+		var destH = animation.yDef*animation.frameH;
+
+		var sourceX = this.frameW * this.currentX;
+		var sourceY = this.frameH * this.currentY;
+
+		context.drawImage(this.image, sourceX, sourceY, animation.frameW,
+				animation.frameH, x, y, destW, destH);
+
+		if(loop == 'default'){
+			if (currentY < destY){
+				if (currentX <= cols)
+					currentX += 1;
+				else{
+					currentX = 1;
+					currentY +=1;
+				}
+
+			}
+			else if (currentY == destY){
+				if(currentX < destX)
+					currentX += 1;
+				else{
+					currentX = startX;
+					currentY = startY;
+				}
+			}
+		}
+		else if (loop == 'reverse'){
+			if (dir == 1){
+				if (currentY < destY){
+					if (currentX <= cols)
+						currentX += 1;
+					else{
+						currentX = 1;
+						currentY +=1;
+					}
+
+				}
+				else if (currentY == destY){
+					if(currentX < destX)
+						currentX += 1;
+					else{
+						if(currentX > 1){
+							currentX -= 1;
+							dir == -1;
+						}
+						else{
+							currentX = cols;
+							currentY -= 1;
+							dir == -1;
+						}
+					}
+				}
+			}
+			else{
+				if (currentY > startY){
+					if (currentX > 1)
+						currentX -= 1;
+					else{
+						currentX = cols;
+						currentY -=1;
+					}
+
+				}
+				else if (currentY == startY){
+					if(currentX > startX)
+						currentX -= 1;
+					else{
+						if(currentX < cols){
+							currentX += 1;
+							dir = 1;
+						}
+						else{
+							currentX = 1;
+							currentY += 1;
+							dir == 1;
+						}
+					}
+				}
+			}
+		}
+	};
+	imageObj.src = AnimationManager.image;
 };
-
-
